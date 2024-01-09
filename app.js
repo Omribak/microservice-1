0eceb754f676c6ba30b6d3ae24c7f015ceabfa16
Ben Cohen <codebrojects@gmail.com>
2024-01-09 20:36:40 +0200

    upload files for microservice one

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = 3001
+
+const makeRequest = async (req, res) =>{
+    const response = await axios.get('http://localhost:3000/microserviceonename?firstname=omri');
+    console.log(response.data)
+}
+makeRequest()
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
