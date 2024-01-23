7766308a6fcabceac7ee75c5e75dda84aa8df0dd
Omribak <97708552+Omribak@users.noreply.github.com>
2024-01-23 10:37:05 +0200

    New Commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const axios = require('axios')
-const app = express()
-const port = 3001
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 3001;
 
-const makeRequest = async (req, res) =>{
-  const response = await axios.get('http://localhost:3000/microserviceonename?firstname=omri');
-  console.log(response.data)
-}
+const makeRequest = async (req, res) => {
+  const response = await axios.get(
+    "http://localhost:3000/microserviceonename?firstname=omri"
+  );
+  console.log(response.data);
+};
 makeRequest();
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example Micro Service 1 listening on port ${port}`);
+});
